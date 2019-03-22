8c4ff06d3b8359d08ad8a5e0cddd460a2986da8b
Aamir <ars3753669@gmail.com>
2019-03-22 16:36:46 +0500

    removing google creds

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ const config = {
     google:{
         type:"oauth2",
         user:"ars3753669@gmail.com",
-        clientId:"741480805956-lblhpiv5tmcjsjrj004hjtbum1ensr2k.apps.googleusercontent.com",
-        clientSecret:"TN8-IHTzVuxba4O1fFfqaj3R",
-        refreshToken:"1/uv2KV0kkmIrCEhv6uV4K_bs1C9b2ufdbhKfXKfVB8RkehPWTWfEmtOEI4zroSQmm"
+        clientId:"*********************************************************",
+        clientSecret:"*****************************************************",
+        refreshToken:"*****************************************************"
     }
 }
 module.exports = config;
